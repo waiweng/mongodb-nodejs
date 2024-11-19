65f543387e93ea86263ddbd55206ddf72ea0332a
waiweng84 <65702453+waiweng84@users.noreply.github.com>
2024-11-19 19:50:19 +1100

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // Require MongoDB language driver
 const { MongoClient } = require("mongodb")
 
+
 // Set the value of uri to your Atlas connection string.
 const uri = 'mongodb+srv://waiweng84:3dS4NAlIJLShs0TC@test-cluster1.vp6si.mongodb.net/?retryWrites=true&w=majority&appName=test-Cluster1'
 
 // Create the MongoClient instance
 const client = new MongoClient(uri)
+
+// Inserting a bank account information in the accounts collection name in the bank database
+const dbname = "bank"
+const collection_name = "accounts"
+const accountsCollection = client.db(dbname).collection(collection_name)
+
+const sampleAccount = {
+ account_holder: "Linus Torvalds",
+ account_id: "MDB829001337",
+ account_type: "checking",
+ balance: 50352434,
+}
+
+// Document used as a filter for the find() method
+const documentsToFind = { balance: { $gt: 4700 } }
 
 // Establishes a connection to the database using the MongoClient instance
 const main = async () => {
@@ -15,6 +31,14 @@ const main = async () => {
       // list out all the databases in the cluster
       const dbs = await client.db().admin().listDatabases()
       console.table(dbs.databases)
+      // insertOne method is used here to insert the sampleAccount document
+      let result = await accountsCollection.insertOne(sampleAccount)
+      console.log(`Inserted document: ${result.insertedId}`)
+      // find() method is used here to find documents that match the filter
+      let findAccount = accountsCollection.find(documentsToFind)
+      let docCount = accountsCollection.countDocuments(documentsToFind)
+      await findAccount.forEach((doc) => console.log(doc))
+      console.log(`Found ${await docCount} documents`)
    } catch (error) {
       console.error(error)
    } finally {
